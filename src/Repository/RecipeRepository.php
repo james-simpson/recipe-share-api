ceb7410bcc2a424d83d637691b0b73b67f276648
James Simpson <james.simpson@ovoenergy.com>
2021-06-27 16:59:10 +0100

    Upgrade packages

--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -4,12 +4,12 @@ namespace App\Repository;
 
 use App\Entity\Recipe;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class RecipeRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Recipe::class);
     }
